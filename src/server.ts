1c6a5b0ecc1c2fed52731fcaaec59caf8c89cd89
Daniel Hessel <hesseldaniel21@gmail.com>
2021-10-14 19:00:36 -0300

    server started

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.get('/', (request: Request, response: Response) => {
+  return response.send('Olá Mundo!')
+});
+
+server.listen('3333', () => {
+  console.log('🚀 Server running on port 3333!');
+});
